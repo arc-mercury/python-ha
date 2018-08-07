8df704b7df0a5259d2c1d30f83984a5b541b962e
Holland G (server) <holland@hollandgibson.com>
2018-08-06 20:05:02 -0400

    more work...

---
+++ honeywell-api.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+import ConfigParser
+import MySQLdb
+import base64
+import requests
+
+config = ConfigParser.RawConfigParser()
+config.read('keys.conf')
+
+db = MySQLdb.connect(host=config.get('DEFAULT','url'),
+			user=config.get('DEFAULT','username'),
+			passwd=config.get('DEFAULT','password'),
+			db=config.get('DEFAULT','db'))
+
+cur = db.cursor()
+
+cur.execute("SELECT code.code FROM code order by creation_time DESC LIMIT 1;")
+
+activekey = 'ERROR'
+
+for row in cur.fetchall():
+	activekey = row[0]
+
+db.close()
+
+appkey = config.get('HONEYWELL','apikey')
+appsecret = config.get('HONEYWELL','apisecret')
+decodedstring = appkey+":"+appsecret
+b64key = base64.b64encode(decodedstring)
+
+r = requests.get('https://api.honeywell.com/oauth2/token', headers={
